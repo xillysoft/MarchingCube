1b25e43a812acbabb5fcaf3c36fa119dcfe1a115
赵小健 <zhaoxiaojian@zhaoxiaojianmacbookair.local>
2016-03-23 10:17:43 +0800

    Initial Commit

---
+++ test_marching_cube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_marching_cube
+//
+//  Created by 赵小健 on 3/23/16.
+//  Copyright © 2016 赵小健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_marching_cube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_marching_cube
+//
+//  Created by 赵小健 on 3/23/16.
+//  Copyright © 2016 赵小健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
